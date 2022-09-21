354cd6c5fed95a23aac49b8f2a73e288be66a3ac
Leonardo Ribeiro <leon_fritas@hotmail.com>
2022-09-21 09:02:36 -0400

    Atualização

--- src/js/tela.js
+++ src/js/tela.js
@@ -7,7 +7,7 @@ function getMovie(movieName) {
                               <img class="movie-img "src="https://image.tmdb.org/t/p/original/${movieData.results[0].poster_path}">
                               <p class="movie-text">${movieData.results[0].overview}</p>
                               <p class="movie-data">${movieData.results[0].release_date}</p>
-                              <button id="voltar" class="botao-volta" onclick="volta()">Voltar</button>
+                              <button id="voltar" class="botao-volta" onclick="volta()">Back</button>
                           </div>`;
     
     document.querySelector(".painel").innerHTML = movieInfo;
